4bd196bba8d92e13b469c5b02f307b96f575c375
Sebastian Daza <sebastian.daza@gmail.com>
2023-01-10 10:31:20 +0100

    update deploy yml

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
@@ -10,6 +9,3 @@ $(document).ready(function() {
     });
     $('a').removeClass('waves-effect waves-light');
 });
-=======
-$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")}),$("a").removeClass("waves-effect waves-light")});
->>>>>>> bbd6c03 (update [ci skip])
